b708ad252782f655d35da51522b6f48a26acf525
daviromao <davi.romao0210@gmail.com>
2023-03-16 20:07:21 -0300

    feat: create multiply and invert functions

--- bigint.c
+++ bigint.c
@@ -13,6 +13,40 @@ typedef struct {
     BigInt remainder;
 } BigIntDivision;
 
+BigInt invert_bigint(BigInt a){
+    short temp;
+    for(int i = 0; i < a.size/2; i++){
+        temp = a.digits[i];
+        a.digits[i] = a.digits[a.size-i-1];
+        a.digits[a.size-i-1] = temp;
+    }
+
+    return a;
+}
+
+BigInt multiply_bigint(BigInt a, BigInt b){
+    BigInt c;
+
+    c.size = MAX_DIGITS;
+    memset(c.digits, 0, sizeof(c.digits));
+
+    for(int i=0; i<a.size; i++){
+        for(int j=0; j<b.size; j++){
+            c.digits[i+j] += a.digits[a.size-i-1] * b.digits[b.size-j-1];
+            c.digits[i+j+1] += c.digits[i+j] / 10;
+            c.digits[i+j] %= 10;
+        }
+    }
+
+    while(c.digits[c.size-1] == 0 && c.size > 1){
+        c.size--;
+    }
+
+    c = invert_bigint(c);
+
+    return c;
+}
+
 BigInt read_bigint(){
     BigInt b;
 
