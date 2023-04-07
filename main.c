54af4698d8898931122a990e72bb84992ca44252
daviromao <davi.romao0210@gmail.com>
2023-04-07 19:26:03 -0300

    feat: main meu

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
-#include "bigint.h"
 #include <stdio.h>
+#include "libs/rsa.c"
+
+void menu(){
+    printf("1 - Criar chaves publica\n");
+    printf("2 - Criptografar\n");
+    printf("3 - Descriptografar\n");
+    printf("4 - Sair\n\n");
+    printf("Digite a opcao desejada: ");
+    int opcao;
+    scanf("%d", &opcao);
+    scanf("%*c");
+
+    switch(opcao){
+        case 1:
+            generate_public_key();
+            break;
+        case 2:
+            encrypt();
+            break;
+        case 3:
+            decrypt();
+            break;
+        case 4:
+            return;
+            break;
+        default:
+            printf("Opcao invalida\n");
+            break;
+    }
+
+}
 
 int main(){
-    BigInt a = read_bigint();
-    printf("a = %d", a.size);
+    menu();
+    return 0;
 }
